51d5238c93604d1e9f2417b7f02718b5c3f157ce
TepidmishA <perep.main@mail.ru>
2026-03-02 13:08:19 +0300

    implement tasks functions

---
+++ include/tasks.h
@@ -0,0 +1,11 @@
+// Copyright 2022 UNN-CS
+#ifndef INCLUDE_TASKS_H_
+#define INCLUDE_TASKS_H_
+#include "circle.h"
+#include <utility>
+
+double task1();
+
+std::pair<double, double> task2();
+
+#endif  // INCLUDE_TASKS_H_
---
+++ src/tasks.cpp
@@ -0,0 +1,29 @@
+// Copyright 2022 UNN-CS
+#include <cstdint>
+#include "tasks.h"
+
+double task1()
+{
+    double radius = 6378.1 * 1000;  // radius of the Earth in meters
+
+    Circle c(radius);
+    c.setFerence(c.getFerence() + 1);
+
+    return c.getRadius() - radius;
+}
+
+std::pair<double, double> task2()
+{
+    double radius = 3.0;            // radius of the pool  in meters
+    double width = 1.0;             // width of the road around the pool  in meters
+    double priceRoad = 1000.0;      // price per square meter
+    double priceFence = 2000.0;     // price per meter
+
+    Circle pool(radius);
+    Circle road(radius + width);
+
+    double costRoad = (road.getArea() - pool.getArea()) * priceRoad;
+    double costFence = road.getFerence() * priceFence;
+
+    return { costRoad, costFence };
+}
