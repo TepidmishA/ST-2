d65ad928da16a08ce085305941ad0ba712340217
TepidmishA <perep.main@mail.ru>
2026-03-02 12:35:00 +0300

    implement class Circle

--- include/circle.h
+++ include/circle.h
@@ -1,7 +1,27 @@
 // Copyright 2022 UNN-CS
 #ifndef INCLUDE_CIRCLE_H_
 #define INCLUDE_CIRCLE_H_
+#define _USE_MATH_DEFINES 
 #include <cstdint>
+#include <cmath>
 
+
+class Circle {
+private:
+    double radius;
+    double ference;
+    double area;
+
+public:
+    Circle(double radius_);
+
+    void setRadius(double radius_);
+    void setFerence(double ference_);
+    void setArea(double area_);
+
+    double getRadius() const;
+    double getFerence() const;
+    double getArea() const;
+};
 
 #endif  // INCLUDE_CIRCLE_H_
--- src/circle.cpp
+++ src/circle.cpp
@@ -1,4 +1,51 @@
 // Copyright 2022 UNN-CS
 #include <cstdint>
+#include <stdexcept>
 #include "circle.h"
 
+
+Circle::Circle(double radius_) {
+    setRadius(radius_);
+}
+
+void Circle::setRadius(double radius_) {
+    if (radius_ < 0)
+        throw std::invalid_argument("Radius must be non-negative");
+
+    radius = radius_;
+
+    ference = 2 * M_PI * radius;
+    area = M_PI * radius * radius;
+}
+
+void Circle::setFerence(double ference_) {
+    if (ference_ < 0)
+        throw std::invalid_argument("Ference must be non-negative");
+
+    ference = ference_;
+    
+    radius = ference / (2 * M_PI);
+    area = M_PI * radius * radius;
+}
+
+void Circle::setArea(double area_) {
+    if (area_ < 0)
+        throw std::invalid_argument("Area must be non-negative");
+
+    area = area_;
+    
+    radius = std::sqrt(area / M_PI);
+    ference = 2 * M_PI * radius;
+}
+
+double Circle::getRadius() const { 
+    return radius; 
+}
+
+double Circle::getFerence() const { 
+    return ference; 
+}
+
+double Circle::getArea() const { 
+    return area; 
+}
